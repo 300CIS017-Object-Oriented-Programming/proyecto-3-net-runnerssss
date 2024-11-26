b5a2770bd992801a9aba50365fdbc73cbd5f55ce
Alexis Trujillo <alexis.trujillo.231@gmail.com>
2024-11-25 23:42:11 -0500

    MOD: Muestra el dataframe de programas académicos en la UI

--- pages/02_Filtros.py
+++ pages/02_Filtros.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+from src.model.Lectura_Archivos import leer_excel
+from src.model.Lectura_Archivos import filtrar_duplicados
+from src.util.Settings import Settings
 
 
 def filtros(controlador):
@@ -42,4 +45,23 @@ def filtros(controlador):
     else:
         st.success(f"Rango seleccionado: {start_year} - {end_year}")
 
+        # Botón de continuar que solo aparece si el rango es válido
+        if st.button("Continuar"):
+            # Guardamos el rango en el controlador
+            controlador.start_year = start_year
+            controlador.end_year = end_year
+            settings = Settings()
+            ruta_inscritos = settings.files['inscritos'] + str(controlador.start_year) + ".xlsx"
+            # Lee el archivo
+
+            df = leer_excel(ruta_inscritos, columnas=settings.get_columns())
+            # Aplica el filtro de duplicados
+            df_filtrado = filtrar_duplicados(df)
+            # Crear un contenedor expandible para mostrar el DataFrame
+            st.dataframe(
+                df_filtrado,
+                width=None,  # Ancho automático
+                height=500  # Altura fija de 500 píxeles
+            )
+# Ejecutar la función
 filtros(st.session_state.controlador)
