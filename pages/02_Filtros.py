f4c0a45e465039a37b9cbbb1de47b0e3135570ba
Alexis Trujillo <alexis.trujillo.231@gmail.com>
2024-11-25 21:45:13 -0500

    MOD: Corrige archivos de inputs

--- pages/02_Filtros.py
+++ pages/02_Filtros.py
@@ -8,13 +8,13 @@ def filtros(controlador):
 
     tipo_filtro = st.radio(
         "Selecciona el tipo de filtro",
-        ["Por Año", "Por Palabra"]
+        ["Rango de Años", "Palabras Clave"]
     )
 
-    if tipo_filtro == "Por Año":
+    if tipo_filtro == "Rango de Años":
         st.subheader("Filtrar por Año")
         anio = st.select_slider("Selecciona el año para filtrar",
-                                options=["2021", "2022", "2023"],
+                                options=["2020","2021", "2022", "2023"],
                                 value="2021")
         archivo = st.session_state.archivo_subido
         df_filtrado = filtrar_por_anio(archivo, anio)
@@ -27,7 +27,7 @@ def filtros(controlador):
             st.warning(f"No se encontraron resultados para el año {anio}")
 
     else:
-        st.subheader("Filtrar por Palabra")
+        st.subheader("Palabras Clave")
         entrada_usuario = st.text_input("Escribe la palabra para filtrar:")
         columna = st.text_input("Indica la columna a filtrar (por defecto: Nombre):",
                                 value="Nombre")
--- src/util/Settings.py
+++ src/util/Settings.py
@@ -1,25 +1,46 @@
 import os
+
+
 class Settings:
     # Ruta base para los archivos
     def __init__(self):
-        self.BASE_PATH = os.getcwd()
+        self.BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.inputs_dir = os.path.join(self.BASE_PATH, 'docs', 'inputs')
         self.outputs_dir = os.path.join(self.BASE_PATH, 'docs', 'outputs')
         self.files = {
-            'admitidos': os.path.join(self.inputs_dir, 'Admitidos'),
-            'graduados': os.path.join(self.inputs_dir, 'Graduados'),
-            'inscritos': os.path.join(self.inputs_dir, 'Inscritos'),
-            'matriculados': os.path.join(self.inputs_dir, 'Matriculados'),
-            'matriculados_primer': os.path.join(self.inputs_dir, 'Matriculados Primer Curso')
+            'admitidos': os.path.join(self.inputs_dir, 'admitidos'),
+            'graduados': os.path.join(self.inputs_dir, 'graduados'),
+            'inscritos': os.path.join(self.inputs_dir, 'inscritos'),
+            'matriculados': os.path.join(self.inputs_dir, 'matriculados'),
+            'matriculados_primer': os.path.join(self.inputs_dir, 'matriculadosprimersemestre')
         }
 
+        # Columnas que se quieren leer de los archivos
+        self.columns_to_read = [
+            'Código de la Institución',
+            'IES PADRE',
+            'Institución de Educación Superior (IES)',
+            'Principal o Seccional',
+            'ID Sector IES',
+            'Sector IES',
+            'ID Caracter IES',
+            'Caracter IES',
+            'Código del departamento (IES)',
+            'Departamento de domicilio de la IES',
+            'Código del Municipio (IES)',
+            'Municipio de domicilio de la IES'
+        ]
+
         # Delimitador para archivos CSV
-        DELIMITADOR = ';'
-        COLUMNAS_INFO_CONSOLIDADOS = 8
-        DATOS_ACADEM_DEMOGRAF = 4
-        COLUMNA_13 = 13
-        COLUMNA_12 = 12
-        FILAS_RESTANTES = 3
+        self.DELIMITADOR = ';'
+        self.COLUMNAS_INFO_CONSOLIDADOS = 8
+        self.DATOS_ACADEM_DEMOGRAF = 4
+        self.COLUMNA_13 = 13
+        self.COLUMNA_12 = 12
+        self.FILAS_RESTANTES = 3
 
     def get_output_path(self, filename):
         return os.path.join(self.outputs_dir, filename)
+
+    def get_columns(self):
+        return self.columns_to_read
